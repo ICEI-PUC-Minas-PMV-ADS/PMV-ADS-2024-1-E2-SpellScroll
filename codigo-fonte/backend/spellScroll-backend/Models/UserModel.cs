6fd2f2d052c33caad0924624002fac9c07163d85
ArthurSouzaC <arthursouza.info@gmail.com>
2024-05-09 19:05:44 -0300

    feat: implemented User CRUD (without connection to database)

---
+++ codigo-fonte/backend/spellScroll-backend/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace spellScroll_backend;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    // Outras propriedades
+}
---
+++ codigo-fonte/backend/spellScroll-backend/Services/MySql.cs
@@ -0,0 +1,20 @@
+﻿using MySqlConnector;
+
+namespace spellScroll_backend;
+
+public class MySql
+{
+
+    private static readonly MySqlConnection connection = new(new MySqlConnectionStringBuilder
+    {
+        Server = "spellscrollpuc.mysql.database.azure.com",
+        UserID = "spellscroll",
+        Password = "Pucm1n@s",
+        SslMode = MySqlSslMode.Required,
+    }.ConnectionString);
+
+    public MySqlConnection GetConnection()
+    {
+        return connection;
+    }
+}
