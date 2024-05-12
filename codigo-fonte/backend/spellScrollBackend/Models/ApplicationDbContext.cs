0fb38dc0fae5c5991548b1df2437acf6e9651239
juan-20 <juanalvesandrade@gmail.com>
2024-05-12 20:23:21 -0300

    adding spells into the back-end

--- codigo-fonte/backend/spellScrollBackend/Models/ApplicationDbContext.cs
+++ codigo-fonte/backend/spellScrollBackend/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using spellScrollBackend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace app_web_backend.Models
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Spell> Spells { get; set; }
     }
 }
