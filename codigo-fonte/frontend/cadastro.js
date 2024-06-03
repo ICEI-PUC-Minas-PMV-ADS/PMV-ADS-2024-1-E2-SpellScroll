0620f453cf4264e58265a55f17ef6d88a86e8802
kianeramalho <91065947+kianeramalho@users.noreply.github.com>
2024-06-03 19:55:31 -0300

    Add files via upload

---
+++ codigo-fonte/frontend/cadastro.js
@@ -0,0 +1,5 @@
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cadastro realizado com sucesso!');
+    window.location.href = 'index.html';
+});
