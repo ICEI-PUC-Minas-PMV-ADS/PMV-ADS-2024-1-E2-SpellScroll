ca0edb7b3924316ea11d0569758aea12393db7fb
juan-20 <juanalvesandrade@gmail.com>
2024-06-09 23:37:20 -0300

    Last changes

---
+++ codigo-fonte/backend/Spells/Migrations/20240610014319_M16-changingDB.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spells.Migrations
+{
+    public partial class M16changingDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spell",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Classes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Concentration = table.Column<bool>(type: "bit", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reach = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ritual = table.Column<bool>(type: "bit", nullable: false),
+                    School = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Somatic = table.Column<bool>(type: "bit", nullable: false),
+                    SpellName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Verbal = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spell", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FavoriteSpells",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SpellId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteSpells", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FavoriteSpells_Spell_SpellId",
+                        column: x => x.SpellId,
+                        principalTable: "Spell",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FavoriteSpells_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteSpells_SpellId",
+                table: "FavoriteSpells",
+                column: "SpellId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteSpells_UsuarioId",
+                table: "FavoriteSpells",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteSpells");
+
+            migrationBuilder.DropTable(
+                name: "Spell");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
